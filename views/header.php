b37805d9a886a2ae7d4b6402df5a78e841772cc3
Saresarah <s80612075@gmail.com>
2026-05-12 11:09:06 +0000

    m

--- views/header.php
+++ views/header.php
@@ -15,34 +15,26 @@
             <a href="aide.php">Aide</a>
         </nav>
 
-    <nav>
-        <a href="../index.php">Accueil</a>
-        <a href="liste_concours.php">Concours</a>
-        <!-- <a href="">Ministères</a> -->
-        <a href="contact.php">Contactez-nous</a>
-        <a href="aide.php">Aide</a>
-    </nav>
-
-            <!-- utilisateur NON connecté -->
-            <div id="guest-buttons" class="guest-button">
-                <a class="btn-primary" href="inscription.php">Créer un compte</a>
-                <a class="btn-secondary" href="connexion.php">Se connecter</a>
-            </div>
-
-            <!-- utilisateur connecté -->
-            <div class="account-wrapper" id="account-wrapper" style="display:none;">
-                <button class="btn-secondary" onclick="toggleMenu()">
-                    Mon compte <i class="fa fa-chevron-down"></i>
-                </button>
-
-                <div class="account-menu" id="accountMenu">
-                    <a href="profil.php" id="profilLink"><i class="fa fa-user"></i> Mon profil</a>
-                    <a href="resultat.php"><i class="fa fa-chart-bar"></i> Mes résultats</a>
-                    <a href="#" style="color: red;" onclick="logout()"><i class="fa fa-sign-out"></i> Se déconnecter</a>
-                </div>
-            </div>
-
+        <!-- utilisateur NON connecté -->
+        <div id="guest-buttons" class="guest-button">
+            <a class="btn-primary" href="inscription.php">Créer un compte</a>
+            <a class="btn-secondary" href="connexion.php">Se connecter</a>
         </div>
+
+        <!-- utilisateur connecté -->
+        <div class="account-wrapper" id="account-wrapper" style="display:none;">
+            <button class="btn-secondary" onclick="toggleMenu()">
+                Mon compte <i class="fa fa-chevron-down"></i>
+            </button>
+
+            <div class="account-menu" id="accountMenu">
+                <a href="profil.php" id="profilLink"><i class="fa fa-user"></i> Mon profil</a>
+                <a href="resultat.php"><i class="fa fa-chart-bar"></i> Mes résultats</a>
+                <a href="#" style="color: red;" onclick="logout()"><i class="fa fa-sign-out"></i> Se déconnecter</a>
+            </div>
+        </div>
+
+    </div>
 
     </div>
 
